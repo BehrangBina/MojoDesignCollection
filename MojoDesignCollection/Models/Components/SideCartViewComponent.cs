de3a5f74180489827646e14dfcf714064af93200
Behrang Bina <behrangbina@hotmail.com>
2020-09-20 20:51:32 +1000

    refactor side cart p1

---
+++ MojoDesignCollection/Models/Components/SideCartViewComponent.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MojoDesignCollection.Models.Components
+{
+    public class SideCartViewComponent : ViewComponent
+    {
+        private Cart cart;
+
+        public SideCartViewComponent(Cart cartService)
+        {
+            cart = cartService;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View(cart);
+        }
+    }
+}
